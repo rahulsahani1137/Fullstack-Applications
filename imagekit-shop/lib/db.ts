bce78f64176fda12796f578d21dfd0e953d39d84
Rahul Sahani <yourahul1917@gmail.com>
2025-03-30 13:49:11 +0530

    feat: image selling webapp initilaized

---
+++ imagekit-shop/lib/db.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+    throw new Error("Check your database connection string");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
+
+export async function connectToDatabase() {
+    if(cached.conn) {
+        return cached.conn
+    }
+}
