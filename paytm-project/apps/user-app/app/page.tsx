f87629220ffe910fc1330f1f1377631fbaab9ab3
Rahul Sahani <yourahul1917@gmail.com>
2025-04-03 21:32:25 +0530

    updates

---
+++ paytm-project/apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from '@repo/ui/card'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Card title={"Card"} href="/"  children={""}/>
+    </div>
+  )
+}
+
+export default Home
