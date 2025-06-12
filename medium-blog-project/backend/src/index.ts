a5323976d9e07a4aa2ab6f97cf5fcff561e147d1
Rahul Sahani <66432121+rahulsahani1137@users.noreply.github.com>
2025-06-12 18:56:28 +0530

    feat:social-events-app: Created a monrepo with pnpm, added backend and React components for the app

--- medium-blog-project/backend/src/index.ts
+++ medium-blog-project/backend/src/index.ts
@@ -2,8 +2,22 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+	return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/signin', () => {
+
+})
+
+app.post('/api/v1/blog', () => {
+})
+
+app.put('/api/v1/blog', () => {
+})
+
+app.get('/api/v1/blog/:id', () => {
+
 })
 
 export default app
